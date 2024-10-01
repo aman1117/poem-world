8ad205de004402e9f0a1a36a81e4cf10a8627eb4
aman1117 <arl0817osho@gmail.com>
2024-10-01 21:41:06 +0000

    Refactor app.controller.ts and add cats module and service

---
+++ src/cats/schemas/cat.schema.ts
@@ -0,0 +1,17 @@
+// src/cats/schemas/cat.schema.ts
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Cat extends Document {
+    @Prop({ required: true })
+    name: string;
+
+    @Prop()
+    age: number;
+
+    @Prop()
+    breed: string;
+}
+
+export const CatSchema = SchemaFactory.createForClass(Cat);
