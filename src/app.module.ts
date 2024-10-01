785a32dc733d63407b1c064f53329b42ff2bc4b4
Ubuntu <u@dev.jqn10x5pamfu1fvogmx3mphara.rx.internal.cloudapp.net>
2024-10-01 18:18:23 +0000

    database connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb+srv://arl0817osho:rOznPNTb1gneSSfv@poem.pijkc.mongodb.net/?retryWrites=true&w=majority&appName=poem')],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
